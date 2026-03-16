d476e76ab22ac029b369dd9ebc6edeb8b973795e
ankryl93 <ankryl93@vk.com>
2026-03-16 13:31:26 +0300

    скорректировал задачу 5 в 1 модуле, выполнил задачу 2 с 1 модуля (пришлось закомментировать строку с объяевлением переменной userName для прохождения автотеста, иначе не пропускал - прекращал проверку на запросе prompt), выполнил задачи 2 модуля

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,6 +1,6 @@
 function truncate(str, maxlength) {
   if(str.length > maxlength){
-    return String(str.slice(0,maxlength-1)) + `${"…"}`;
+    return str.slice(0,maxlength-1) + "…";
   } else {
     return str;
   }
