de97abb0645f020e07beb710ddaa829e41f7e139
ankryl93 <ankryl93@vk.com>
2026-04-02 06:03:24 +0300

    доделал задачу с первого модуля, а также полностью модули 3 и 4

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,11 @@
 function getMinMax(str) {
-  // ваш код...
+  let splited = str.split(" ");
+  let numbers = splited
+    .map(item => parseFloat(item))
+    .filter(num => !isNaN(num));
+  
+  return {
+    min: Math.min(...numbers),
+    max: Math.max(...numbers)
+  };
 }
